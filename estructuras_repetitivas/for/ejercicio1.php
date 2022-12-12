eb7b305a65c0d858eccc5a857f61d93810c09d69
Oscar <oscaralexander29@gmail.com>
2022-12-12 13:46:44 -0600

    nueva tarea

---
+++ estructuras_repetitivas/for/ejercicio1.php
@@ -0,0 +1,27 @@
+<?php
+/* Dado el sueldo de 20 trabajadores, considere un aumento del 15% a cada uno de ellos, 
+si su sueldo es inferior a $800. Imprima el sueldo con el aumento incorporado. */
+$sueldo = 0;
+$aumento = 0;
+$acum_total = 0;
+
+echo "DETERMINAR EL AUMENTO DE SUELDO DE 20 TRABAJADORES".'<br>';
+for($i=1; $i<=20; $i++)
+{
+    echo "----------------------------------------------------".'<br>';
+    echo "Ingrese el sueldo del trabajador: $ " . $i;
+    $sueldo= readline();
+    if ($sueldo<800)
+    {
+        $aumento = $sueldo * 0.15;
+    }
+    $acum_total += $sueldo + $aumento;
+    /* Imprimir Datos */
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~".'<br>';
+    echo "Sueldo: $ " . $sueldo .'<br>';
+    echo "Aumento: $ " . $aumento .'<br>';
+    echo "Sueldo con aumento: $ " . $sueldo + $aumento.'<br>'.'<br>';
+}/* Fin FOR */
+echo "*********************** PLANILLA ***************************";
+echo "TOTAL A PAGAR POR EL JEFE: $ " . $acum_total;
+?>
